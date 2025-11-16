9a4066e18f6a6431e31124e3c1a675af8d13e1d2
Tickloop <pulkit.arya.tech@gmail.com>
2025-11-15 20:51:12 -0500

    added shadCN ui elements

--- ui/src/ui/index.jsx
+++ ui/src/ui/index.jsx
@@ -1,1 +1,9 @@
+import { clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs) {
+  return twMerge(clsx(inputs))
+}
+
 export { Input } from './input';
+export { Button } from './button';
